8287d3dd3cbdee238bd47923ddc5dd66231ad473
ericforbes <ericforbes91@gmail.com>
2017-07-06 23:49:40 -0400

    config manager class helper for config setup in each git repo. similiar to .git file

--- postreview/logger.py
+++ postreview/logger.py
@@ -1,8 +1,18 @@
 import logging
+import sys
 from configprocesser import get_configuration
 
 def create_logger():
     logging.basicConfig(filename=get_configuration('logfile'), filemode='w', format='%(asctime)s - %(levelname)s - %(message)s')
-    logger = logging.getLogger()
-    logger.setLevel(logging.getLevelName('DEBUG'))
-    return logger
+
+    root = logging.getLogger()
+    root.setLevel(logging.getLevelName('DEBUG'))
+
+    #Add handler for standard output any errors+
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(logging.getLevelName('ERROR'))
+    formatter = logging.Formatter('%(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    root.addHandler(ch)
+
+    return root
