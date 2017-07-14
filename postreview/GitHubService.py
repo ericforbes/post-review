d0f2234b142948b75ad986a78e7ee080eeb756b6
ericforbes <ericforbes91@gmail.com>
2017-07-13 22:09:53 -0400

    adding GitHub Merge Request API w/ error handing

--- postreview/GitHubService.py
+++ postreview/GitHubService.py
@@ -3,69 +3,120 @@ import sys
 
 #Dependencies
 from urlparse import urljoin
-import requests
+import requests #pip based
 import json
+import re
 
 class GitHubService(BaseService):
     SERVICE_NAME = "github"
-    GIT_CONFIG_API_KEY = "postreview.github.oath"
+    GIT_CONFIG_API_KEY = "postreview.github.token"
     API = "https://api.github.com"
     CLIENT_ID = '40435bdcc83a625b6c9b'
     CLIENT_SECRET = '81703a26b85a31127071c6bbbc54ac37ea970df3'
 
-    def issue_pull_request(self, message):
-        owner, project_name = self._owner_of_repo(self)
+    def issue_pull_request(self, obj):
+
+        try:
+            remote_url = obj['remote_url']
+            merge_message = obj['message']
+            api_token = obj['api_token']
+        except KeyError:
+            self.logger.fatal("GitHubService::issue_pull_request has incorrect parameters @%s" % obj)
+            sys.exit()
+
+        owner, project_name = self._repo_information(remote_url)
 
         params = {
-            'title': message,
+            'title': merge_message,
             'head': self.source_branch,
             'base': self.parent_branch
         }
+        print params
+        #/repos/:owner/:repo/pulls
+        #"Authorization: token OAUTH-TOKEN"
+        # TODO  before EVERY REQUEST... BEFORE!!.. check if current API key is valid. add a @handler at top to do this.
+        #  for example. i run this tool and it creates the key. i go into github and delete it. 
+        url = urljoin(self.API, 'repos/%s/%s/pulls' % (owner, project_name))
+        print url
+        headers = {"Authorization": "token %s" % api_token}
+        res = {}
+
+        try:
+            res = requests.post(
+                url,
+                headers = headers,
+                data = json.dumps(params)
+                )
+        except Exception as e:
+            self.logger.fatal(e)
+            sys.exit()
 
 
-        print self.remote_branch
-        print "GitHub: issue pull request"
+        json_response = json.loads(res.text)
+        print json_response
+        print res
+        if res.status_code >= 400:
 
-    def _owner_of_repo(self):
+            try:
+                err_msg = json_response['errors'][0]['message']
+            except:
+                err_msg = ''
+
+            self.logger.fatal("Error: Could not create merge request, %s: %s" % (json_response['message'], err_msg))
+            sys.exit()
+        
+        print "GitHub: issue pull request from %s to %s" % (self.source_branch, self.parent_branch)
+        print j
+
+    def _repo_information(self, remote_origin):
         match_str = ""
-        matchObj = re.search(r'git@github.com:(?P<ssh_git_url_path>.*)/|https://github.com/(?P<https_git_url_path>.*)/', self.remote_origin_url)
-        if matchObj.group('ssh_git_url_path'):
-            match_str = matchObj.group('ssh_git_url_path').split("/")
-        elif matchObj.group('https_git_url_path'):
-            match_str = matchObj.group('https_git_url_path').split("/")
+        matchObj = re.search(r'git@github.com:(?P<ssh_owner_project>.*)\.git|https://github.com/(?P<https_owner_project>.*)\.git', remote_origin)
+        if matchObj.group('ssh_owner_project'):
+            print matchObj.group('ssh_owner_project')
+            match_str = matchObj.group('ssh_owner_project').split("/")
+        elif matchObj.group('https_owner_project'):
+            print matchObj.group('https_owner_project')
+            match_str = matchObj.group('https_owner_project').split("/")
         else:
             self.logger.error("Error: Unable to determine owner of GitHub repository")
+            sys.exit()
+
+        if not match_str[0] and not match_str[1]:
+            self.logger.error("Error: Unable to find both owner and project from giturl")
             sys.exit()
 
         owner = match_str[0]
         project_name = match_str[1]
         return (owner, project_name)
 
-    def _request_token(self, u, p):
+    def _request_token(self, user, pw):
         url = urljoin(self.API, 'authorizations/clients/%s' % self.CLIENT_ID)
         #TODO: Add Fingerprint
         params = {
             'note': 'post-review cli utility (github, gitlab, etc)',
             'client_secret': self.CLIENT_SECRET,
-            'scope': ['public_repo', 'repo']
+            'scopes': ['public_repo', 'repo']
         }
+        print params
         print url
         res = requests.put(
             url,
-            auth = (u,p),
+            auth = (user,pw),
             data = json.dumps(params)
             )
-
+        print res.text
         j = json.loads(res.text)
         print j
+
         if res.status_code >= 400:
             msg = j.get('message', 'UNDEFINED ERROR (no error description from server)')
-            self.logger.error(msg)
+            self.logger.error("Fatal: Could not request API token      Reason: %s " % msg)
             sys.exit()
         if not j['token'] and j['hashed_token']: #TODO: token is not in config, but its created. so its lost in the ether, create a new one
             #TODO: Will need to delete current token, then re-request. Make sure no infinite loop occurs.
             self.logger.fatal('Fatal: Please revoke the token then re-run cmd: https://github.com/settings/applications')
         elif j['token']:
+            self.api_token = j['token']
             return j['token']
         else:
             self.logger.fatal('Fatal: Eronious error fetching github credentials')
